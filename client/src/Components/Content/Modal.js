610ba67cfe5dd44893ae720282c715c7fbabcb23
nktnlg <2eleven92@gmail.com>
2022-12-10 19:07:08 -0300

    finishing

--- client/src/Components/Content/Modal.js
+++ client/src/Components/Content/Modal.js
@@ -33,20 +33,24 @@ const Modal = props => {
         <div className="modal" onClick={props.closeModal}>
             <div className="modal-content" onClick={e => e.stopPropagation()}>
              <div className="modal-header">
+                {props.edit ? <div className="modal-title-margin"><input className="modal-title-input" value={taskName} onChange={(e)=>setTaskName(e.target.value)}></input></div>:
                 <h3 className="modal-title">{taskName}</h3>
+                }
              </div>
                 {props.edit ? 
                 <textarea className="modal-body-edit" value={taskDescription} onChange={(e)=>setTaskDescription(e.target.value)}></textarea> : 
                 <textarea className="modal-body" value={taskDescription}></textarea> }
              <div className="modal-footer">
-
-                {props.edit ? <>
+                <div className="modal-buttons">
+                {props.edit ? <div className="modal-buttons">
                     <button className="modal-button" onClick={()=>{saveChanges()}}>Save</button>
                     <button className="modal-button" onClick={()=>{cancelChanges()}}>Cancel</button>
-                </>
+                </div>
                  :
                 <button className="modal-button" onClick={()=>{props.setEdit(true)}}>Edit</button>}
+
                 <button className="modal-button" onClick={props.closeModal}>Close</button>
+                </div>
              </div>
             </div>
         </div>
